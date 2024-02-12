1c0ae6960c2f6d3e19618ebf563ba0c9e1e84c4d
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-02-12 18:07:45 +0530

    paddings updated

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ function Navbar() {
     return (
         <nav className={styles.navbar}>
             <ul className={styles.nav_links}>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Skills</li>
-                <li>Experience</li>
-                <li>Contact</li>
+                <li className={`${styles.nav_link} ${styles.active_link}`}>About</li>
+                <li className={styles.nav_link}>Projects</li>
+                <li className={styles.nav_link}>Skills</li>
+                <li className={styles.nav_link}>Experience</li>
+                <li className={styles.nav_link}>Contact</li>
             </ul>
         </nav>
     );
