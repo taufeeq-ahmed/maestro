3afd2630c40ccdb3850dc7f069e983e0d26c00c0
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-02-20 10:03:18 +0530

    meta data updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,8 +8,8 @@ import styles from './layout.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-    title: 'Maestro Taufeeq Ahmed',
-    description: 'Taufeeq Ahmed\' developer portfolio',
+    title: 'Taufeeq Ahmed',
+    description: 'Taufeeq Ahmed\'s developer portfolio',
     keywords: 'taufeeq ahmed maestro cogoport software full-stack',
 };
 
