353172a0bd90b4cba33fa64943f22ba7a0ad572f
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-02-20 18:47:39 +0530

    grammer corrections

--- components/DeveloperDescription/DeveloperDescription.jsx
+++ components/DeveloperDescription/DeveloperDescription.jsx
@@ -8,18 +8,16 @@ function DeveloperDescription() {
     return (
         <section className={styles.developer_description}>
             <p>
-                Back in 2014, I decided go deep into cutome themes on Nova launcher in android to make my launcher system completely customizable and fell into
-                the abyss of digital design. From there I went on to learn basic HTML, CSS
-                and Javascript and as you can see .... here I am a professional Full Stack Engineer.
+                In 2014, I decided to delve deep into custom themes on Nova Launcher in Android to make my launcher system completely customizable. This led me into the abyss of digital design. From there, I proceeded to learn basic HTML, CSS, and JavaScript, and as you can see... here I am, a professional Full Stack Engineer.
             </p>
             <p>
-                My main focus these days is building products at Cogoport. I enjoy building software where design and engineering meet. Things that look good but are also built well under the hood. In my free time, I&apos;ve also explored various developement environments such as Jetpack Compose, SwiftUi, ElectronJS and many more.
+                My main focus these days is building products at Cogoport. I enjoy creating software where design and engineering converge — things that look good but are also well-built under the hood. In my free time, I&apos;ve also explored various development environments such as Jetpack Compose, SwiftUI, ElectronJS, and many more.
             </p>
             <p>
                 When I&apos;m not at the computer, I&apos;m usually watching a lot of movies, playing Badminton
             </p>
             <p>
-                Here are the languages and frameworks I worked with
+                Here are the languages and frameworks I have worked with
                 {' :'}
             </p>
             <div className={styles.skills}>
