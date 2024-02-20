1b923f547846fa83cefd055c9b21a3040d1fb9b7
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-02-20 09:56:47 +0530

    font style updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,11 @@
-import { Montserrat } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import React from 'react';
 import { Toaster } from 'sonner';
 
 import styles from './layout.module.css';
 
-const montserrat = Montserrat({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
     title: 'Maestro Taufeeq Ahmed',
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={montserrat.className}>
+            <body className={inter.className}>
 
                 <Toaster
                     richColors
